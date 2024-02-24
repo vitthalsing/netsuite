92c26f6603baaa8cf2d454c9691d39580ddb4369
vitthalsing <vitthalsing@gmail.com>
2024-02-24 11:19:24 +0000

    hello world

---
+++ helloWorld-v2.js
@@ -0,0 +1,39 @@
+/**
+ *@NApiVersion 2.0
+ *@NScriptType ClientScript
+ */
+
+
+define(['N/ui/dialog'],
+
+    function (dialog) {
+
+        function helloWorld() {
+
+            var options = {
+                title: 'Hello!',
+                message: 'Hello, World! This is Vitthal'
+            };
+
+            try {
+
+                dialog.alert(options);
+
+                log.debug({
+                    title: 'Success',
+                    details: 'Alert displayed successfully'
+                });
+
+            } catch (e) {
+
+                log.error({
+                    title: e.name,
+                    details: e.message
+                });
+            }
+        }
+
+        return {
+            pageInit: helloWorld
+        };
+    });
